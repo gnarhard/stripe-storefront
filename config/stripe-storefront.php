77df6c31f33e34d935e0d4631b7c795c63b62a8e
Grayson Erhard <gcerhard@gmail.com>
2026-01-29 12:01:41 -0700

    feat: create media migration and update products table for image_urls

--- config/stripe-storefront.php
+++ config/stripe-storefront.php
@@ -2,7 +2,6 @@
 
 return [
     'downloads-storage-disk' => env('STRIPE_STOREFRONT_DOWNLOADS_STORAGE_DISK', 'product-downloads'),
-    'products-storage-disk' => env('STRIPE_STOREFRONT_PRODUCTS_STORAGE_DISK', 'products'),
     'stripe' => [
         'live_secret' => env('STRIPE_LIVE_SECRET'),
         'test_secret' => env('STRIPE_TEST_SECRET'),
